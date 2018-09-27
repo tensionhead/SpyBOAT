5e1776847ade9b8886ea8bb6fe86426ee6d2568e
Jelle Scholtalbers <j.scholtalbers@gmail.com>
2018-09-27 09:41:28 +0200

    Add local dependencies

---
+++ src/run_local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# example command
+python ana_movie.py --mov_dir ../test_data/ --mov_name twosmall_hom_sines.tif --phase_out phase_out.tif --period_out period_out.tif --power_out power_out.tif --dt 10 --Tmin 50 --Tmax 220 --nT 20
